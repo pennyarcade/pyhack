2ff79050917cb3cb2882d0945d3fc1fd1aff6a1e
Martin Tönnishoff <martin.toennishoff@buizcore.com>
2016-01-27 17:06:53 +0100

    fix lots of prospector issues

--- pyhack.py
+++ pyhack.py
@@ -1,67 +1,87 @@
-import lib.colorama, lib.colorama.ansi
-import sys, os, time, random, platform
+"""
+Pyhack
+
+Python roguelike game using (almost) only standard library calls
+Console only for now
+Attempts to be cross platform
+"""
+
+# import sys
+import os
+# import time
+# import random
+import platform
+# import lib.colorama
+# import lib.colorama.ansi
 from crt import Crt
 
 
 def main():
-    '''
-        main function
-    '''
+    """
+    main function
 
+    @todo: does much to much right now
+    """
+    screen = Crt()
     screen.clrScr()
 
     try:
         from msvcrt import getch #For windows
     except ImportError: #For linux and maybe mac...
         def getch():
+            """
+            read single character from keyboard
 
-            import sys, tty, termios
-            fd = sys.stdin.fileno()
-            old_settings = termios.tcgetattr(fd)
+            duplicates behaviour of msvcrt on Unix sytems
+            """
+            import sys
+            import tty
+            import termios
+
+            file_descriptor = sys.stdin.fileno()
+            old_settings = termios.tcgetattr(file_descriptor)
             try:
                 tty.setraw(sys.stdin.fileno())
-                ch = sys.stdin.read(1)
+                character = sys.stdin.read(1)
             finally:
-                termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
-            return ch
+                termios.tcsetattr(file_descriptor, termios.TCSADRAIN, old_settings)
+            return character
 
-
-    def pressedkey():
-        return getch()
 
               # 0   1   2   3   4   5   6   7   8   9  10   11  12  13  14  15
-    room = {1:['#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#'], # ^
-            2:['#','.','.','.','.','.','.','.','.','.','.','.','.','.','.','#'], # |
-            3:['#','.','.','.','.','.','.','.','.','#','#','#','#','.','.','#'], # |
-            4:['#','.','.','.','.','.','.','#','.','#','.','.','.','.','.','#'], # |
-            5:['#','.','.','.','.','.','@','#','.','#','.','.','#','.','.','#'], # |
-            6:['#','.','.','.','.','.','.','#','.','.','.','.','#','.','.','#'], # x
-            7:['#','.','.','.','.','#','#','#','.','#','#','.','#','.','.','#'], # |
-            8:['#','.','.','.','.','.','.','.','.','.','#','#','#','.','.','#'], # |
-            9:['#','.','.','.','.','.','.','.','.','.','.','.','.','.','.','#'], # |
-           10:['#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#']} # |
+    room = {1:['#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#'],  # ^
+            2:['#', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '#'],  # |
+            3:['#', '.', '.', '.', '.', '.', '.', '.', '.', '#', '#', '#', '#', '.', '.', '#'],  # |
+            4:['#', '.', '.', '.', '.', '.', '.', '#', '.', '#', '.', '.', '.', '.', '.', '#'],  # |
+            5:['#', '.', '.', '.', '.', '.', '@', '#', '.', '#', '.', '.', '#', '.', '.', '#'],  # |
+            6:['#', '.', '.', '.', '.', '.', '.', '#', '.', '.', '.', '.', '#', '.', '.', '#'],  # x
+            7:['#', '.', '.', '.', '.', '#', '#', '#', '.', '#', '#', '.', '#', '.', '.', '#'],  # |
+            8:['#', '.', '.', '.', '.', '.', '.', '.', '.', '.', '#', '#', '#', '.', '.', '#'],  # |
+            9:['#', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '#'],  # |
+           10:['#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#']} # |
             # <------------------------------y--------------------------------->
 
-    stuff  = {'wall'  :  "#",
-              'player':  "@",
-              'empty' :  ".",
-              'money' :  "$",
-              'chest' :  "C"}
-    #potion = ['hp','xp','dmg']
-    #weapon = ['sword','nuclear bomb','TNT']
-
+    stuff = {'wall' : "#",
+             'player' : "@",
+             'empty' : ".",
+             'money' : "$",
+             'chest' : "C"}
+    # potion = ['hp','xp','dmg']
+    # weapon = ['sword','nuclear bomb','TNT']
 
 
     pos = [] # 0 is X,1 is Y
 
 
     def gamemap(room):
-        for i in range(1,len(room)+1):
+        """draw game mask"""
+        for i in range(1, len(room) + 1):
             print "".join(room[i])
 
 
     def player_pos(pos):
-        for i in range(1,len(room)+1):
+        """get player position"""
+        for i in range(1, len(room) + 1):
             if stuff['player'] in room[i]:
                 x_axis = i
                 y_axis = room[i].index(stuff['player'])
@@ -71,7 +91,8 @@ def main():
 
 
     def updater(pos, room):
-        if platform.system() =='Windows':
+        """update game screen"""
+        if platform.system() == 'Windows':
             os.system('cls')
         elif platform.system() == 'Linux':
             os.system('clear') #For linux.. I don't know what to do about mac :/
@@ -79,33 +100,36 @@ def main():
         player_pos(pos)
 
 
-    def up(ditcioary,inst_replace,inst_player):
+    def move_up(ditcioary, inst_replace, inst_player):
+        """move player up"""
         (ditcioary[pos[0]]).pop(pos[1])
-        (ditcioary[pos[0]]).insert(pos[1],inst_replace)
-        (ditcioary[pos[0]-1]).pop(pos[1])
-        (ditcioary[pos[0]-1]).insert(pos[1],inst_player)
+        (ditcioary[pos[0]]).insert(pos[1], inst_replace)
+        (ditcioary[pos[0] - 1]).pop(pos[1])
+        (ditcioary[pos[0] - 1]).insert(pos[1], inst_player)
 
 
-    def down(ditcioary,inst_replace,inst_player):
+    def move_down(ditcioary, inst_replace, inst_player):
+        """move player down"""
         (ditcioary[pos[0]]).pop(pos[1])
-        (ditcioary[pos[0]]).insert(pos[1],inst_replace)
-        (ditcioary[pos[0]+1]).pop(pos[1])
-        (ditcioary[pos[0]+1]).insert(pos[1],inst_player)
+        (ditcioary[pos[0]]).insert(pos[1], inst_replace)
+        (ditcioary[pos[0] + 1]).pop(pos[1])
+        (ditcioary[pos[0] + 1]).insert(pos[1], inst_player)
 
 
-    def left(ditcioary,inst_replace,inst_player):
+    def move_left(ditcioary, inst_replace, inst_player):
+        """move player left"""
         (ditcioary[pos[0]]).pop(pos[1])
-        (ditcioary[pos[0]]).insert(pos[1],inst_replace)
-        (ditcioary[pos[0]]).pop(pos[1]-1)
-        (ditcioary[pos[0]]).insert(pos[1]-1,inst_player)
+        (ditcioary[pos[0]]).insert(pos[1], inst_replace)
+        (ditcioary[pos[0]]).pop(pos[1] - 1)
+        (ditcioary[pos[0]]).insert(pos[1] - 1, inst_player)
 
 
-    def right(ditcioary,inst_replace,inst_player):
+    def move_right(ditcioary, inst_replace, inst_player):
+        """move player right"""
         (ditcioary[pos[0]]).pop(pos[1])
-        (ditcioary[pos[0]]).insert(pos[1],inst_replace)
-        (ditcioary[pos[0]]).pop(pos[1]+1)
-        (ditcioary[pos[0]]).insert(pos[1]+1,inst_player)
-
+        (ditcioary[pos[0]]).insert(pos[1], inst_replace)
+        (ditcioary[pos[0]]).pop(pos[1] + 1)
+        (ditcioary[pos[0]]).insert(pos[1] + 1, inst_player)
 
 
     updater(pos, room)
@@ -114,29 +138,29 @@ def main():
         pressedkey = getch()
 
         if pressedkey == 'w' or pressedkey == 'W':
-            if room[pos[0]-1][pos[1]] !=  stuff['wall']:
-                up(room, stuff['empty'], stuff['player'])
+            if room[pos[0]-1][pos[1]] != stuff['wall']:
+                move_up(room, stuff['empty'], stuff['player'])
                 updater(pos, room)
                 print pos
             else:
                 print "Bump! Wall : up"
         elif pressedkey == 's' or pressedkey == 'S':
-            if room[pos[0]+1][pos[1]] !=  stuff['wall']:
-                down(room,stuff['empty'],stuff['player'])
+            if room[pos[0]+1][pos[1]] != stuff['wall']:
+                move_down(room, stuff['empty'], stuff['player'])
                 updater(pos, room)
                 print pos
             else:
                 print "Bump! wall : down"
         elif pressedkey == 'a' or pressedkey == 'A':
-            if room[pos[0]][pos[1]-1] !=  stuff['wall']:
-                left(room,stuff['empty'], stuff['player'])
+            if room[pos[0]][pos[1]-1] != stuff['wall']:
+                move_left(room, stuff['empty'], stuff['player'])
                 updater(pos, room)
                 print pos
             else:
                 print "Bump! wall : left"
         elif pressedkey == 'd' or pressedkey == 'D':
-            if room[pos[0]][pos[1]+1] !=  stuff['wall']:
-                right(room,stuff['empty'], stuff['player'])
+            if room[pos[0]][pos[1]+1] != stuff['wall']:
+                move_right(room, stuff['empty'], stuff['player'])
                 updater(pos, room)
                 print pos
             else:
@@ -147,10 +171,7 @@ def main():
 
 
 
-
-
 # run main function
 if __name__ == "__main__":
     #colorama.init('autoreset=True')
-    screen= Crt()
     main()
--- testkey.py
+++ testkey.py
@@ -1,84 +1,94 @@
 #!/usr/bin/env python
-
 # this solution will work only in Windows, as msvcrt is a Windows only package
+
+"""
+Test script for keyboard capturing
+
+Uses threads to scan for keypresses
+"""
 
 import thread
 import time
 
-
-keybuffer= list()
-lock= thread.allocate_lock()
+keybuffer = list()
+lock = thread.allocate_lock()
 running = True
+
 
 try:
     from msvcrt import getch  # try to import Windows version
 
     def keypress():
+        """keypress windows"""
         global keybuffer, running
 
         char = getch()
 
         lock.aquire()
-        if ord(char)==0 or ord(char)==224: #Special keys
+        if ord(char) == 0 or ord(char) == 224: #Special keys
             keybuffer.push(getch())
         keybuffer.push(char)
 
         while len(keybuffer) > 100:
             keybuffer.pop(0)
 
-        running=False
+        running = False
         lock.release()
 
-
-
 except ImportError:
-
     def getch():   # define non-Windows version
-        import sys, tty, termios
-        fd = sys.stdin.fileno()
-        old_settings = termios.tcgetattr(fd)
+        """getch linux"""
+        import sys
+        import tty
+        import termios
+        file_descriptor = sys.stdin.fileno()
+        old_settings = termios.tcgetattr(file_descriptor)
         try:
             tty.setraw(sys.stdin.fileno())
-            ch = sys.stdin.read(1)
+            character = sys.stdin.read(1)
         finally:
-            termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
-        return ch
+            termios.tcsetattr(file_descriptor, termios.TCSADRAIN, old_settings)
+        return character
 
 
     def keypress():
+        """keypress linux"""
         global keybuffer, running
 
         # get special char combinations on linux up to 5 byte
-        a=[]
-        a.push(getch())
-        if ord(a[0])==27:
-            a.push(getch())
-            if ord(a[1])==91:
-                a.push(getch())
-                if (ord(a[2])>=49 and ord(a[2])<=54) or ord(a[2])==91:
-                    a.push(getch())
-                    if ord(a[3])>=48 and ord(a[3])<=57:
-                        a.push(getch())
+        character_array = []
+        character_array.push(getch())
+        if ord(character_array[0]) == 27:
+            character_array.push(getch())
+            if ord(character_array[1]) == 91:
+                character_array.push(getch())
+                if (
+                    ord(character_array[2]) >= 49 and ord(character_array[2]) <= 54
+                ) or ord(character_array[2]) == 91:
+                    character_array.push(getch())
+                    if ord(character_array[3]) >= 48 and ord(character_array[3]) <= 57:
+                        character_array.push(getch())
 
         lock.aquire()
         # add to keyboard buffer in reverse order to facilitate popping o stack
-        while len(a) > 0:
-            keybuffer.push(a.pop())
+        while len(character_array) > 0:
+            keybuffer.push(character_array.pop())
 
         # limit buffer size by discarting overflow
         while len(keybuffer) > 100:
             keybuffer.pop(0)
 
-        running= False
+        running = False
         lock.release()
 
 
 
-def scanForKeys():
+def scan_for_keys():
+    """repeatedly restart key scanning"""
     global running
 
     # wait for key in separate thread
-    running= True
+    running = True
     thread.start_new_thread(keypress, ())
 
     # scan forever
@@ -93,17 +103,19 @@ def scanForKeys():
         time.sleep(0.1)
 
 
-def keyPressed():
+def key_pressed():
+    """has a key been pressed i.e. keybuffer is not empty"""
     lock.aquire()
-    value= len(keybuffer) > 0
+    value = len(keybuffer) > 0
     lock.release()
     return value
 
 
-def readKey():
+def read_key():
+    """fetch a key from the keyboard buffer"""
     try:
         lock.aquire()
-        value= keybuffer.pop()
+        value = keybuffer.pop()
         lock.release()
 
         return value
