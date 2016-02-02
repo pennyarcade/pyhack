aff2d122cb09d3ec84b117c3dbe6ff70e9cb7bef
Martin Tönnishoff <martin.toennishoff@buizcore.com>
2016-02-02 16:55:38 +0100

    refactor game scriot into class

--- pyhack.py
+++ pyhack.py
@@ -11,192 +11,201 @@ import platform
 from crt import Crt
 
 
-def gamemap(room, screen):
-    """draw game mask"""
-    for i in range(1, len(room) + 1):
-        screen.write_ln("".join(room[i]))
+class PyHack(object):
+    """main game class"""
+    def __init__(self):
+        super(PyHack, self).__init__()
+
+        self.screen = Crt()
+        self.screen.clr_scr()
+
+                # 0   1    2    3    4    5    6    7
+                # 8   9   10   11   12   13   14   15
+        self.room = {
+            1:[
+                '#', '#', '#', '#', '#', '#', '#', '#',   # ^
+                '#', '#', '#', '#', '#', '#', '#', '#'
+            ],
+            2:[
+                '#', '.', '.', '.', '.', '.', '.', '.',   # |
+                '.', '.', '.', '.', '.', '.', '.', '#'
+            ],
+            3:[
+                '#', '.', '.', '.', '.', '.', '.', '.',   # |
+                '.', '#', '#', '#', '#', '.', '.', '#'
+            ],
+            4:[
+                '#', '.', '.', '.', '.', '.', '.', '#',   # |
+                '.', '#', '.', '.', '.', '.', '.', '#'
+            ],
+            5:[
+                '#', '.', '.', '.', '.', '.', '@', '#',   # |
+                '.', '#', '.', '.', '#', '.', '.', '#'
+            ],
+            6:[
+                '#', '.', '.', '.', '.', '.', '.', '#',   # x
+                '.', '.', '.', '.', '#', '.', '.', '#'
+            ],
+            7:[
+                '#', '.', '.', '.', '.', '#', '#', '#',   # |
+                '.', '#', '#', '.', '#', '.', '.', '#'
+            ],
+            8:[
+                '#', '.', '.', '.', '.', '.', '.', '.',   # |
+                '.', '.', '#', '#', '#', '.', '.', '#'
+            ],
+            9:[
+                '#', '.', '.', '.', '.', '.', '.', '.',   # |
+                '.', '.', '.', '.', '.', '.', '.', '#'
+            ],
+            10:[
+                '#', '#', '#', '#', '#', '#', '#', '#',   # |
+                '#', '#', '#', '#', '#', '#', '#', '#'
+            ]
+        }
+        # <---------------------y---------------------------->
+
+        self.stuff = {
+            'wall' : "#",
+            'player' : "@",
+            'empty' : ".",
+            'money' : "$",
+            'chest' : "C"
+        }
+        # self.potion = ['hp','xp','dmg']
+        # self.weapon = ['sword','nuclear bomb','TNT']
+
+        self.pos = [] # 0 is X,1 is Y
+
+        self.updater()
 
 
-def player_pos(pos, room, stuff):
-    """get player position"""
-    for i in range(1, len(room) + 1):
-        if stuff['player'] in room[i]:
-            x_axis = i
-            y_axis = room[i].index(stuff['player'])
-            del pos[:]
-            pos.append(x_axis)
-            pos.append(y_axis)
+    def updater(self):
+        """update game screen"""
+        # @todo: use Crt class to clear screen
+        if platform.system() == 'Windows':
+            os.system('cls')
+        elif platform.system() == 'Linux':
+            os.system('clear') #For linux.. I don't know what to do about mac :/
+        self.gamemap()
+        self.player_pos()
 
 
-def updater(pos, room, stuff, screen):
-    """update game screen"""
-    if platform.system() == 'Windows':
-        os.system('cls')
-    elif platform.system() == 'Linux':
-        os.system('clear') #For linux.. I don't know what to do about mac :/
-    gamemap(room, screen)
-    player_pos(pos, room, stuff)
+    def gamemap(self):
+        """draw game mask"""
+        for i in range(1, len(self.room) + 1):
+            self.screen.write_ln("".join(self.room[i]))
 
 
-def move_up(ditcioary, inst_replace, inst_player, pos):
-    """move player up"""
-    (ditcioary[pos[0]]).pop(pos[1])
-    (ditcioary[pos[0]]).insert(pos[1], inst_replace)
-    (ditcioary[pos[0] - 1]).pop(pos[1])
-    (ditcioary[pos[0] - 1]).insert(pos[1], inst_player)
+    def player_pos(self):
+        """get player position"""
+        for i in range(1, len(self.room) + 1):
+            if self.stuff['player'] in self.room[i]:
+                x_axis = i
+                y_axis = self.room[i].index(self.stuff['player'])
+                del self.pos[:]
+                self.pos.append(x_axis)
+                self.pos.append(y_axis)
 
 
-def move_down(ditcioary, inst_replace, inst_player, pos):
-    """move player down"""
-    (ditcioary[pos[0]]).pop(pos[1])
-    (ditcioary[pos[0]]).insert(pos[1], inst_replace)
-    (ditcioary[pos[0] + 1]).pop(pos[1])
-    (ditcioary[pos[0] + 1]).insert(pos[1], inst_player)
+    def game_loop(self):
+        """main game loop"""
+        while True:
+            pressedkey = self.screen.read_key()
+
+            if pressedkey == 'w' or pressedkey == 'W':
+                self.do_up()
+            elif pressedkey == 's' or pressedkey == 'S':
+                self.do_down()
+            elif pressedkey == 'a' or pressedkey == 'A':
+                self.do_left()
+            elif pressedkey == 'd' or pressedkey == 'D':
+                self.do_right()
+            elif pressedkey == '#':
+                quit()
 
 
-def move_left(ditcioary, inst_replace, inst_player, pos):
-    """move player left"""
-    (ditcioary[pos[0]]).pop(pos[1])
-    (ditcioary[pos[0]]).insert(pos[1], inst_replace)
-    (ditcioary[pos[0]]).pop(pos[1] - 1)
-    (ditcioary[pos[0]]).insert(pos[1] - 1, inst_player)
+    def do_up(self):
+        """process up movement"""
+        if self.room[self.pos[0]-1][self.pos[1]] != self.stuff['wall']:
+            self.move_up(self.stuff['empty'], self.stuff['player'])
+            self.updater()
+            self.screen.write_ln(self.pos)
+        else:
+            self.screen.write_ln("Bump! Wall : up")
 
 
-def move_right(ditcioary, inst_replace, inst_player, pos):
-    """move player right"""
-    (ditcioary[pos[0]]).pop(pos[1])
-    (ditcioary[pos[0]]).insert(pos[1], inst_replace)
-    (ditcioary[pos[0]]).pop(pos[1] + 1)
-    (ditcioary[pos[0]]).insert(pos[1] + 1, inst_player)
+    def do_down(self):
+        """process down movement"""
+        if self.room[self.pos[0]+1][self.pos[1]] != self.stuff['wall']:
+            self.move_down(self.stuff['empty'], self.stuff['player'])
+            self.updater()
+            self.screen.write_ln(self.pos)
+        else:
+            self.screen.write_ln("Bump! wall : down")
 
 
-def do_up(pos, room, stuff, screen):
-    """process up movement"""
-    if room[pos[0]-1][pos[1]] != stuff['wall']:
-        move_up(room, stuff['empty'], stuff['player'], pos)
-        updater(pos, room, stuff, screen)
-        screen.write_ln(pos)
-    else:
-        screen.write_ln("Bump! Wall : up")
+    def do_right(self):
+        """process right movement"""
+        if self.room[self.pos[0]][self.pos[1]+1] != self.stuff['wall']:
+            self.move_right(self.stuff['empty'], self.stuff['player'])
+            self.updater()
+            self.screen.write_ln(self.pos)
+        else:
+            self.screen.write_ln("Bump! wall : right")
 
 
-def do_down(pos, room, stuff, screen):
-    """process down movement"""
-    if room[pos[0]+1][pos[1]] != stuff['wall']:
-        move_down(room, stuff['empty'], stuff['player'], pos)
-        updater(pos, room, stuff, screen)
-        screen.write_ln(pos)
-    else:
-        screen.write_ln("Bump! wall : down")
+    def do_left(self):
+        """process right movement"""
+        if self.room[self.pos[0]][self.pos[1]-1] != self.stuff['wall']:
+            self.move_left(self.stuff['empty'], self.stuff['player'])
+            self.updater()
+            self.screen.write_ln(self.pos)
+        else:
+            self.screen.write_ln("Bump! wall : left")
 
 
-def do_right(pos, room, stuff, screen):
-    """process right movement"""
-    if room[pos[0]][pos[1]+1] != stuff['wall']:
-        move_right(room, stuff['empty'], stuff['player'], pos)
-        updater(pos, room, stuff, screen)
-        screen.write_ln(pos)
-    else:
-        screen.write_ln("Bump! wall : right")
+    def move_up(self, inst_replace, inst_player):
+        """move player up"""
+        (self.room[self.pos[0]]).pop(self.pos[1])
+        (self.room[self.pos[0]]).insert(self.pos[1], inst_replace)
+        (self.room[self.pos[0] - 1]).pop(self.pos[1])
+        (self.room[self.pos[0] - 1]).insert(self.pos[1], inst_player)
 
 
-def do_left(pos, room, stuff, screen):
-    """process right movement"""
-    if room[pos[0]][pos[1]-1] != stuff['wall']:
-        move_left(room, stuff['empty'], stuff['player'], pos)
-        updater(pos, room, stuff, screen)
-        screen.write_ln(pos)
-    else:
-        screen.write_ln("Bump! wall : left")
+    def move_down(self, inst_replace, inst_player):
+        """move player down"""
+        (self.room[self.pos[0]]).pop(self.pos[1])
+        (self.room[self.pos[0]]).insert(self.pos[1], inst_replace)
+        (self.room[self.pos[0] + 1]).pop(self.pos[1])
+        (self.room[self.pos[0] + 1]).insert(self.pos[1], inst_player)
 
 
-def game_loop(pos, room, stuff, screen):
-    """main game loop"""
-    while True:
-        pressedkey = screen.read_key()
+    def move_left(self, inst_replace, inst_player):
+        """move player left"""
+        (self.room[self.pos[0]]).pop(self.pos[1])
+        (self.room[self.pos[0]]).insert(self.pos[1], inst_replace)
+        (self.room[self.pos[0]]).pop(self.pos[1] - 1)
+        (self.room[self.pos[0]]).insert(self.pos[1] - 1, inst_player)
 
-        if pressedkey == 'w' or pressedkey == 'W':
-            do_up(pos, room, stuff, screen)
-        elif pressedkey == 's' or pressedkey == 'S':
-            do_down(pos, room, stuff, screen)
-        elif pressedkey == 'a' or pressedkey == 'A':
-            do_left(pos, room, stuff, screen)
-        elif pressedkey == 'd' or pressedkey == 'D':
-            do_right(pos, room, stuff, screen)
-        elif pressedkey == '#':
-            quit()
 
+    def move_right(self, inst_replace, inst_player):
+        """move player right"""
+        (self.room[self.pos[0]]).pop(self.pos[1])
+        (self.room[self.pos[0]]).insert(self.pos[1], inst_replace)
+        (self.room[self.pos[0]]).pop(self.pos[1] + 1)
+        (self.room[self.pos[0]]).insert(self.pos[1] + 1, inst_player)
 
 
 def main():
     """
     main function
 
-    @todo: does much to much right now
+    kick off the game
     """
-    screen = Crt()
-    screen.clr_scr()
 
-            # 0   1    2    3    4    5    6    7
-            # 8   9   10   11   12   13   14   15
-    room = {
-        1:[
-            '#', '#', '#', '#', '#', '#', '#', '#',   # ^
-            '#', '#', '#', '#', '#', '#', '#', '#'
-        ],
-        2:[
-            '#', '.', '.', '.', '.', '.', '.', '.',   # |
-            '.', '.', '.', '.', '.', '.', '.', '#'
-        ],
-        3:[
-            '#', '.', '.', '.', '.', '.', '.', '.',   # |
-            '.', '#', '#', '#', '#', '.', '.', '#'
-        ],
-        4:[
-            '#', '.', '.', '.', '.', '.', '.', '#',   # |
-            '.', '#', '.', '.', '.', '.', '.', '#'
-        ],
-        5:[
-            '#', '.', '.', '.', '.', '.', '@', '#',   # |
-            '.', '#', '.', '.', '#', '.', '.', '#'
-        ],
-        6:[
-            '#', '.', '.', '.', '.', '.', '.', '#',   # x
-            '.', '.', '.', '.', '#', '.', '.', '#'
-        ],
-        7:[
-            '#', '.', '.', '.', '.', '#', '#', '#',   # |
-            '.', '#', '#', '.', '#', '.', '.', '#'
-        ],
-        8:[
-            '#', '.', '.', '.', '.', '.', '.', '.',   # |
-            '.', '.', '#', '#', '#', '.', '.', '#'
-        ],
-        9:[
-            '#', '.', '.', '.', '.', '.', '.', '.',   # |
-            '.', '.', '.', '.', '.', '.', '.', '#'
-        ],
-        10:[
-            '#', '#', '#', '#', '#', '#', '#', '#',   # |
-            '#', '#', '#', '#', '#', '#', '#', '#'
-        ]
-    }
-    # <---------------------y---------------------------->
-
-    stuff = {'wall' : "#",
-             'player' : "@",
-             'empty' : ".",
-             'money' : "$",
-             'chest' : "C"}
-    # potion = ['hp','xp','dmg']
-    # weapon = ['sword','nuclear bomb','TNT']
-
-
-    pos = [] # 0 is X,1 is Y
-
-    updater(pos, room, stuff, screen)
-
-    game_loop(pos, room, stuff, screen)
+    game = PyHack()
+    game.game_loop()
 
 
 # run main function
